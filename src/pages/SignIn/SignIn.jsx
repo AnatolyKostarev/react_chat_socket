938c4da3fb23ee6b6241b3e8aa6a9eb5bc732a73
Anatoly <anatolykostarev@gmail.com>
2022-07-31 02:38:15 +0300

    Refactor SignIn component

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -3,15 +3,27 @@ import { Header } from "../../layouts/Header";
 import { Main } from "../../layouts/Main";
 import { Footer } from "../../layouts/Footer";
 import { Container } from "../../components/Container/Container";
+import { DecorationLine } from "../../components/DecorationLine/DecorationLine";
+import { Title } from "../../components/Title/Title";
 import { FormHeader } from "../../components/FormTitle/FormHeader";
 import { Form } from "../../components/Form/Form";
 import { Input } from "../../components/Input/Input";
 import { Button } from "../../components/Button/Button";
+import s from "./SignIn.module.css";
 
 export const SignIn = ({ title, link, path, indent }) => {
   return (
     <>
-      <Header />
+      <Header>
+        <DecorationLine />
+        <Container>
+          <Title
+            caption="Чат чат"
+            description="Привет, заходи к нам в чат и мы научим тебя 
+программировать на html, css и javascript."
+          />
+        </Container>
+      </Header>
       <Main>
         <Container>
           <FormHeader title={title} link={link} path={path} indent={indent} />
@@ -19,9 +31,7 @@ export const SignIn = ({ title, link, path, indent }) => {
             <Input label="Никнейм" placeholder="Никнейм" />
             <Input type="password" label="Пароль" placeholder="Пароль" />
           </Form>
-          <Button pt="15" pr="26" pb="14" pl="26">
-            Зарегистрироваться
-          </Button>
+          <Button className={s.btn__sign}>Зарегистрироваться</Button>
         </Container>
       </Main>
       <Footer name="Анатолий" />
