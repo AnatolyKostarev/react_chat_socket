471c0e78e51ca96edf437ac534eaacd9c15c87b1
Anatoly <anatolykostarev@gmail.com>
2022-07-31 02:38:33 +0300

    Refactor LogIn component

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -4,14 +4,26 @@ import { Main } from "../../layouts/Main";
 import { Footer } from "../../layouts/Footer";
 import { Container } from "../../components/Container/Container";
 import { FormHeader } from "../../components/FormTitle/FormHeader";
+import { DecorationLine } from "../../components/DecorationLine/DecorationLine";
+import { Title } from "../../components/Title/Title";
 import { Form } from "../../components/Form/Form";
 import { Input } from "../../components/Input/Input";
 import { Button } from "../../components/Button/Button";
+import s from "./LogIn.module.css";
 
 export const LogIn = ({ title, link, path, indent }) => {
   return (
     <>
-      <Header />
+      <Header>
+        <DecorationLine />
+        <Container>
+          <Title
+            caption="Чат чат"
+            description="Привет, заходи к нам в чат и мы научим тебя 
+программировать на html, css и javascript."
+          />
+        </Container>
+      </Header>
       <Main>
         <Container>
           <FormHeader title={title} link={link} path={path} indent={indent} />
@@ -19,9 +31,7 @@ export const LogIn = ({ title, link, path, indent }) => {
             <Input label="Никнейм" placeholder="Никнейм" />
             <Input type="password" label="Пароль" placeholder="Пароль" />
           </Form>
-          <Button pt="15" pr="51" pb="14" pl="50">
-            Войти
-          </Button>
+          <Button className={s.btn__login}>Войти</Button>
         </Container>
       </Main>
       <Footer name="Анатолий" />
