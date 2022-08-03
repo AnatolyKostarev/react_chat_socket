5fbc7dc47e9eefad0aa12b0faba40db8635a9d89
Anatoly <anatolykostarev@gmail.com>
2022-08-04 01:25:35 +0300

    Delete routing

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import * as React from "react";
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
 import { SignIn } from "./pages/SignIn/SignIn";
-import { LogIn } from "./pages/LogIn/LogIn";
 import { ChatRoom } from "./pages/ChatRoom/ChatRoom";
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
-
-  return isAuth ? (
+  return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <SignIn title="Регистрация" link="Вход" path="/login" indent="14" />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <LogIn title="Вход" link="Регистрация" path="/" indent="27" />
-          }
-        />
-        <Route path="/chatroom" element={<ChatRoom title="Общая комната" />} />
-      </Routes>
-    </>
-  ) : (
-    <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <SignIn title="Регистрация" link="Вход" path="/login" indent="14" />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <LogIn title="Вход" link="Регистрация" path="/" indent="27" />
-          }
-        />
-      </Routes>
+      {!isAuth ? (
+        <SignIn setIsAuth={setIsAuth} />
+      ) : (
+        <ChatRoom title="Общая комната" />
+      )}
     </>
   );
 }
