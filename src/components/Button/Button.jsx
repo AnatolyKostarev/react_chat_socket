7b2e10c44b73e7f29ee09653948490a964099b9c
Anatoly <anatolykostarev@gmail.com>
2022-08-04 02:19:34 +0300

    Add default type for Button component (#10)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import clsx from "clsx";
 import s from "./Button.module.css";
 
-export const Button = ({ className, children, onClick }) => {
+export const Button = ({ className, children, onClick, type = "submit" }) => {
   return (
     <div>
-      <button className={clsx(s.btn, className)} onClick={onClick}>
+      <button className={clsx(s.btn, className)} type={type} onClick={onClick}>
         {children}
       </button>
     </div>
