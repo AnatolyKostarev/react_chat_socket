2ee8f429fad8e20119ccae07732eb364180f7d72
Anatoly <anatolykostarev@gmail.com>
2022-08-04 01:21:44 +0300

    Delete routing, add event for span

--- src/components/FormTitle/FormHeader.jsx
+++ src/components/FormTitle/FormHeader.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import clsx from "clsx";
-import { Link } from "react-router-dom";
 import s from "./FormHeader.module.css";
 
-export const FormHeader = ({ className, title, link, path, indent }) => {
+export const FormHeader = ({ className, title, link, indent, onClick }) => {
   return (
     <div className={clsx(s.form__header, className)}>
       <h3 style={{ marginRight: `${indent}px` }}>{title}</h3>
-      <Link to={path}>
-        <span>{link}</span>
-      </Link>
+      <span onClick={onClick}>{link}</span>
     </div>
   );
 };
