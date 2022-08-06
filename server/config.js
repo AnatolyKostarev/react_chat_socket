e1481682c6fa088c170a426fc9087d99406b35ea
Anatoly <anatolykostarev@gmail.com>
2022-08-06 17:55:32 +0300

    Chat room (#11)
    * Replace front into client folder
    
    * Add server folder
    
    * delete

---
+++ server/config.js
@@ -0,0 +1,5 @@
+// разрешенный источник
+export const ALLOWED_ORIGIN = "http://localhost:3000";
+// адрес БД
+export const MONGODB_URI =
+  "mongodb+srv://user_app_chat:Wu8pxZi5.frTe_N@cluster0.kamexwk.mongodb.net/?retryWrites=true&w=majority";
