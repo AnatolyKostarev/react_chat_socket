6e74daef2486c16bf67e70c50ea9e69cdbc30960
Anatoly <anatolykostarev@gmail.com>
2022-10-03 17:57:48 +0300

    Reformat code, change min length property

--- client/src/components/TextArea/TextArea.jsx
+++ client/src/components/TextArea/TextArea.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import clsx from "clsx";
-import s from "./TextArea.module.css";
+import React from 'react'
+import clsx from 'clsx'
+import s from './TextArea.module.css'
 
 export const TextArea = ({
   className,
   rows = 5,
   cols = 63,
-  autoComplete = "off",
+  autoComplete = 'off',
   onChange,
   value,
 }) => {
@@ -16,10 +16,11 @@ export const TextArea = ({
       rows={rows}
       cols={cols}
       autoComplete={autoComplete}
-      minLength="20"
-      maxLength="500"
+      minLength='2'
+      maxLength='500'
       onChange={onChange}
       value={value}
+      placeholder='Ваше сообщение...'
     ></textarea>
-  );
-};
+  )
+}
