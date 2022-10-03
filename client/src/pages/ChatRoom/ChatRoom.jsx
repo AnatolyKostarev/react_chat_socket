1a8b7d9cf5233704f3f31e01e4ded1d4dbbbffda
Anatoly <anatolykostarev@gmail.com>
2022-10-03 17:59:48 +0300

    Reformat code,  add useChat hook to show user messages

--- client/src/pages/ChatRoom/ChatRoom.jsx
+++ client/src/pages/ChatRoom/ChatRoom.jsx
@@ -1,20 +1,24 @@
-import React from "react";
-import { useState } from "react";
-import { Container } from "../../components/Container/Container";
-import { Title } from "../../components/Title/Title";
-import { Header } from "../../layouts/Header";
-import { DecorationLine } from "../../components/DecorationLine/DecorationLine";
-import { Main } from "../../layouts/Main";
-import { UsersList } from "../../components/UsersList/UsersList";
-import { UserMessage } from "../../components/UserMessage/UserMessage";
-import { Form } from "../../components/Form/Form";
-import { TextArea } from "../../components/TextArea/TextArea";
-import { Footer } from "../../layouts/Footer";
-import { Button } from "../../components/Button/Button";
-import s from "./ChatRoom.module.css";
+import React from 'react'
+import { useState } from 'react'
+import { Container } from '../../components/Container/Container'
+import { Title } from '../../components/Title/Title'
+import { Header } from '../../layouts/Header'
+import { DecorationLine } from '../../components/DecorationLine/DecorationLine'
+import { Main } from '../../layouts/Main'
+import { UsersList } from '../../components/UsersList/UsersList'
+import { UserMessage } from '../../components/UserMessage/UserMessage'
+import { Form } from '../../components/Form/Form'
+import { TextArea } from '../../components/TextArea/TextArea'
+import { Footer } from '../../layouts/Footer'
+import { Button } from '../../components/Button/Button'
+import useChat from '../../hooks/useChat'
+import s from './ChatRoom.module.css'
 
 export const ChatRoom = ({ title }) => {
-  const [value, setValue] = useState("Ваше сообщение...");
+  const [value, setValue] = useState('')
+  const { sendMessage, messages, log } = useChat()
+
+  console.log(log)
   return (
     <>
       <Header className={s.header__chat}>
@@ -26,52 +30,33 @@ export const ChatRoom = ({ title }) => {
       <Main className={s.main__chat}>
         <Container>
           <UsersList className={s.users__chat}>
-            <UserMessage>
-              <span className={s.user__marker}></span>
-              <p className={s.user__name}>Васек</p>
-              <p className={s.user__message}>
-                Нет ну SaintCode конечно крутая школа. А Юрий так вообще
-                шикарен, дай бог ему здоровья.
-              </p>
-            </UserMessage>
-            <UserMessage>
-              <span className={s.user__marker}></span>
-              <p className={s.user__name}>Лерочка</p>
-              <p className={s.user__message}>
-                Согласна, вот это он конечно молодец
-              </p>
-            </UserMessage>
-            <UserMessage>
-              <span className={s.user__marker}></span>
-              <p className={s.user__name}>Катенька</p>
-              <p className={s.user__message}>По фактам, АБАЖАЮ его ❤️</p>
-            </UserMessage>
-            <UserMessage>
-              <span className={s.user__marker}></span>
-              <p className={s.user__name}>Катенька</p>
-              <p className={s.user__message}>По фактам, АБАЖАЮ его ❤️</p>
-            </UserMessage>
-            <UserMessage>
-              <span className={s.user__marker}></span>
-              <p className={s.user__name}>Катенька</p>
-              <p className={s.user__message}>По фактам, АБАЖАЮ его ❤️</p>
-            </UserMessage>
-            <UserMessage>
-              <span className={s.user__marker}></span>
-              <p className={s.user__name}>Катенька</p>
-              <p className={s.user__message}>По фактам, АБАЖАЮ его ❤️</p>
-            </UserMessage>
+            {messages.map((elem, index) => (
+              <UserMessage key={index}>
+                <span className={s.user__marker}></span>
+                <p className={s.user__name}>{elem.name}</p>
+                <p className={s.user__message}>{elem.value}</p>
+              </UserMessage>
+            ))}
           </UsersList>
-          <Form className={s.form__chat} onSubmit={(e) => e.preventDefault()}>
+          <Form
+            className={s.form__chat}
+            onSubmit={e => {
+              e.preventDefault()
+              setValue('')
+              sendMessage({ name: localStorage.getItem('user'), value })
+            }}
+          >
             <TextArea
               value={value}
-              onChange={(e) => setValue(e.target.value)}
+              onChange={e => {
+                setValue(e.target.value)
+              }}
             />
             <Button className={s.btn__chat}>Отправить</Button>
           </Form>
         </Container>
       </Main>
-      <Footer className={s.footer__chat} name="Анатолий" />
+      <Footer className={s.footer__chat} name='Анатолий' />
     </>
-  );
-};
+  )
+}
